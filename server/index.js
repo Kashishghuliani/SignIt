81d1764cc195e09c5284670855fdb3609f25ec13
Kashish Ghuliani <kashishghuliani2004@gmail.com>
2025-07-04 17:39:52 +0530

    Fix CORS allowedOrigins for Vercel frontend

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const app = express();
 
 const allowedOrigins = [
   process.env.FRONTEND_URL,     
-  'http://localhost:3000'       
+  'https://sign-it-5656.vercel.app'       
 ];
 
 app.use(cors({
